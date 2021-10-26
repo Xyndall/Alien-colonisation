7b99aba8d4bd252cfb2e0f2ec1cd1f43a0ae185f
Irena <skrzynir@gmail.com>
2021-10-26 22:51:12 +1300

    PeopleProgress
    work done from home

--- Assets/Irena/Scripts/PeopleAi.cs
+++ Assets/Irena/Scripts/PeopleAi.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PeopleAi : MonoBehaviour
 {
+    public float walkingSpeed = 2;
+    public float runningSpeed = 5;
+
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+      GameObject rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.transform.position = new Vector2(walkingSpeed * Time.deltaTime, 0);
     }
 }
--- Assets/Irena/Scripts/PeopleSpawn.cs
+++ Assets/Irena/Scripts/PeopleSpawn.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PeopleSpawn : MonoBehaviour
 {
+    public int peopleMax = 30;
+    int peopleCount;
+    public GameObject peopleSprite;
+
+    public GameObject leftSpawn;
+    public GameObject rightSpawn;
+    
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+          PeopleSpawnOnScreen();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PeopleSpawnOnScreen()
+    }
+
+    void PeopleSpawnOnScreen()
+    {
+        GameObject peopleHolder = new GameObject("People Holder");
+
+        for (int people = 0; people < peopleMax; people++)
+        {
+          GameObject peopleCopy =  Instantiate(peopleSprite, new Vector3(leftSpawn.transform.position.x, leftSpawn.transform.position.y, leftSpawn.transform.position.z), transform.rotation, peopleHolder.transform);
+          peopleCopy.name = "person:" + (people + 1);
+          print("Person copied");
+
+         
+        }
+
+       
     }
 }
