ad45476722bf6cc251045f393021094fe0aef6cf
tyrel <tyrel.ratima@gmail.com>
2021-10-27 15:28:57 +1300

    EnemyAI and sprites
    enemy AI movement, shooting and sprites

---
+++ Assets/Tyrel/Scripts/BulletDestroy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroy : MonoBehaviour
+{
+    public GameObject explosion = null;
+    
+    private void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject explode = Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(explode, 0.5f);
+            Destroy(gameObject);
+        }
+        
+        if (collision.gameObject.CompareTag("Death"))
+        {
+            GameObject explode = Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(explode, 0.5f);
+            Destroy(gameObject);
+        }
+
+
+
+    }
+
+}
---
+++ Assets/Tyrel/Scripts/EnemySpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemy = null;
+    float randx;
+    Vector2 whereToSpawn;
+    public float spawnRate = 3f;
+    float nextSpawn = 0.0f;
+
+    public List<GameObject> Tanks = new List<GameObject>();
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Time.time > nextSpawn)
+        {
+            SpawnTanks();
+
+        }
+
+        if(Tanks.Count >= 10)
+        {
+            spawnRate = 100;
+        }
+
+    }
+
+    void SpawnTanks()
+    {
+        nextSpawn = Time.time + spawnRate;
+        randx = Random.Range(-10, 10);
+        whereToSpawn = new Vector2(randx, transform.position.y);
+        GameObject tanks = Instantiate(enemy, whereToSpawn, Quaternion.identity);
+        Tanks.Add(tanks);
+    }
+
+}
