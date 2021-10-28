e6b9ceb293429d406f741a4ce2dcadfebdde1200
kyle <studio11keyuniverse@gmail.com>
2021-10-28 15:28:51 +1300

    Menus

--- Assets/Kyle/Script/MainMenu.cs
+++ Assets/Kyle/Script/MainMenu.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Text scoretext;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoretext.text = "200";
     }
 
     // Update is called once per frame
