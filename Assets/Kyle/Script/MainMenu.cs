71457380768f4c6f61201e8a61a0c77a7f65cb16
Kyle <studio11keyuniverse@gmail.com>
2021-10-29 11:13:20 +1300

    MainMenu An

--- Assets/Kyle/Script/MainMenu.cs
+++ Assets/Kyle/Script/MainMenu.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Playables;
 
 public class MainMenu : MonoBehaviour
 {
     public Text scoretext;
+    public PlayableDirector startcutscnce;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +24,21 @@ public class MainMenu : MonoBehaviour
 
     public void Playgame()
     {
+        startcutscnce.Play();
+
+        StartCoroutine(WaitforCutscne());
+
+    }
+
+    public void PlaygameWin()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    private IEnumerator WaitforCutscne()
+    {
+        yield return new WaitForSeconds(5f);
         SceneManager.LoadScene(1);
     }
 
