3930a3a4724166e7fe2ba6925eb9364cc2583e28
Phoenix <phoenix.millar.020@gmail.com>
2021-10-29 13:23:47 +1300

    Life System

---
+++ Assets/Phoenix/Scripts/LifeSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LifeSystem : MonoBehaviour
+{
+    public GameObject[] ships;
+    public int life;
+    private bool dead;
+
+    private void Start()
+    {
+        life = ships.Length;
+    }
+
+    public void Update()
+    {
+        if (dead == true)
+        {
+            Debug.Log("Out of Lives");
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public void TakeDamage(int d)
+    {
+        if (life >= 1)
+        {
+            life -= d;
+            Destroy(ships[life].gameObject);
+            if (life < 1)
+            {
+                dead = true;
+            }
+        }
+    }
+}
